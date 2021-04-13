304476a34b2060ae87a35808ecef3cfab3266d71
aymanmahran <55334062+aymanmahran@users.noreply.github.com>
2021-04-13 17:52:40 +0200

    d

--- build/dev-server.js
+++ build/dev-server.js
@@ -84,6 +84,13 @@ devMiddleware.waitUntilValid(() => {
   _resolve()
 })
 
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+	res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
 var server = app.listen(port)
 
 module.exports = {
